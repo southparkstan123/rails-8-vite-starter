7bb7ae7f141f2e25b8f611ff73287739795e02e7
southparkstan123 <southparkstan123@gmail.com>
2025-04-09 13:08:30 +0800

    Set alias path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [
@@ -11,5 +12,10 @@ export default defineConfig({
   ],
   server: {
     hmr: true
-  }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('app/javascript', import.meta.url))
+    },
+  },
 })
